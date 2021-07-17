a6c6922fa953c0f08fcf2db4e31f5d66b777c484
Artem Yakunin <artm.yakunin@gmail.com>
2021-07-17 21:37:10 +0700

    change Main class

--- src/main/java/ru/yakunin/animationsystemofanimals/Main.java
+++ src/main/java/ru/yakunin/animationsystemofanimals/Main.java
@@ -1,6 +1,30 @@
 package ru.yakunin.animationsystemofanimals;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.yakunin.animationsystemofanimals.configurations.ApplicationConfiguration;
+import ru.yakunin.animationsystemofanimals.models.Animal;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        AnimationSystemOfAnimals animationSystemOfAnimals =
+                context.getBean("animationSystemOfAnimals", AnimationSystemOfAnimals.class);
+        List<Animal> herbivoresAnimals = animationSystemOfAnimals.getHerbivoresAnimals();
+        System.out.println("Травоядные животные (Количество: " + herbivoresAnimals.size() + ")");
+//        herbivoresAnimals.forEach(System.out::println);
+        List<Animal> herbivoresOrCarnivoresAndSmallAnimals =
+                animationSystemOfAnimals.getHerbivoresOrCarnivoresAndSmallAnimals();
+        System.out.println("Травоядные или плотоядные и при этом маленькие животные (Количество: "
+                + herbivoresOrCarnivoresAndSmallAnimals.size() + ")");
+//        herbivoresOrCarnivoresAndSmallAnimals.forEach(System.out::println);
+        List<Animal> omnivoresAndNotTallAnimals =
+                animationSystemOfAnimals.getOmnivoresAndNotTallAnimals();
+        System.out.println("Всеядных и не высокие животные (Количество: "
+                + omnivoresAndNotTallAnimals.size() + ")");
+//        omnivoresAndNotTallAnimals.forEach(System.out::println);
+        context.close();
     }
 }

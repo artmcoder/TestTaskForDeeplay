79a9c2f12db700cbda5bccf9930ddadba7bf9c99
Artem Yakunin <artm.yakunin@gmail.com>
2021-07-17 21:35:29 +0700

    add spring configuration

---
+++ src/main/java/ru/yakunin/animationsystemofanimals/configurations/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package ru.yakunin.animationsystemofanimals.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.yakunin.animationsystemofanimals")
+public class ApplicationConfiguration {
+}
